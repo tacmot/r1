4db0ece145751fcc71a18df184d5fddcc708731c
haha <haha@haha.com>
2019-06-17 09:07:44 +0800

    添加输出语句

--- a.java
+++ a.java
@@ -2,6 +2,10 @@ public class a {
 	public static void main(String args[]){
 		System.out.println("ÄãºÃ£¬¿ËÂ¡");
 		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		
 	}
 	
 }
