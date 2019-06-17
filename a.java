1778ef1d96af69cedb70ec1a72cec3bc6c01cb5b
haha <haha@haha.com>
2019-06-17 09:11:05 +0800

    添加语句

--- a.java
+++ a.java
@@ -1,6 +1,9 @@
 public class a {
 	public static void main(String args[]){
 		System.out.println("你好，克隆");
+		System.out.println("你好，克隆");
+		System.out.println("你好，克隆");
+		System.out.println("你好，克隆");
 		System.out.println(1);
 	}
 	
